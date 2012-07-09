befdfe6685edc25b5cc17a1efcea1cf775a2f56e
Michael Whelan <mjmwdev@gmail.com>
2012-07-09 12:03:42 +0100

    Added basic Guard class

---
+++ src/TestStack.Seleno/Guard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestStack.Seleno
+{
+    public static class Guard
+    {
+        public static void Against(bool condition, string errorMessage)
+        {
+            if (condition)
+            {
+                throw new ArgumentException(errorMessage);
+            }
+        }
+    }
+}
