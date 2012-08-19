4b2f427a05a904ae1b53da0fcd26db7021d39ab9
MehdiK <me@mehdi-khalili.com>
2012-08-19 20:50:53 +1000

    Fixed Step 4

--- src/Samples/TestStack.Seleno.Samples.MvcMusicStore.FunctionalTests/Step4/LivingDocumentation.cs
+++ src/Samples/TestStack.Seleno.Samples.MvcMusicStore.FunctionalTests/Step4/LivingDocumentation.cs
@@ -28,8 +28,7 @@ namespace TestStack.Seleno.Samples.MvcMusicStore.FunctionalTests.Step4
         // should be straight to the db, not through the UI
         public void Given_that_I_am_a_logged_in_user()
         {
-            _homePage = new Application()
-                .HomePage
+            _homePage = new HomePage()
                 .Menu
                 .GoToAdminForAnonymousUser()
                 .GoToRegisterPage()
