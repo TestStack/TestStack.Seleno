75ad13afe2a264a046fb3d65ceee89bafb6401cd
MehdiK <me@mehdi-khalili.com>
2012-08-21 13:25:06 +1000

    Removed the now unnecessary teardown

--- src/Samples/MvcMusicStore.FunctionalTests/BrittleTests.cs
+++ src/Samples/MvcMusicStore.FunctionalTests/BrittleTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using TestStack.Seleno.Configuration;
 
@@ -7,64 +6,49 @@ namespace MvcMusicStore.FunctionalTests
 {
     class BrittleTests
     {
-        private IWebDriver _driver;
-
-        [TearDown]
-        public void TeardownTest()
-        {
-            try
-            {
-                _driver.Quit();
-            }
-            catch (Exception)
-            {
-                // Ignore errors if unable to close the browser
-            }
-        }
-
         [Test]
         public void Can_buy_an_Album_when_registered()
         {
-            _driver = SelenoApplicationRunner.Host.Browser;
-            _driver.Navigate().GoToUrl(_driver.Url);
-            _driver.FindElement(By.LinkText("Admin")).Click();
-            _driver.FindElement(By.LinkText("Register")).Click();
-            _driver.FindElement(By.Id("UserName")).Clear();
-            _driver.FindElement(By.Id("UserName")).SendKeys("HJSimpson");
-            _driver.FindElement(By.Id("Email")).Clear();
-            _driver.FindElement(By.Id("Email")).SendKeys("chunkylover53@aol.com");
-            _driver.FindElement(By.Id("Password")).Clear();
-            _driver.FindElement(By.Id("Password")).SendKeys("!2345Qwert");
-            _driver.FindElement(By.Id("ConfirmPassword")).Clear();
-            _driver.FindElement(By.Id("ConfirmPassword")).SendKeys("!2345Qwert");
-            _driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
-            _driver.FindElement(By.LinkText("Disco")).Click();
-            _driver.FindElement(By.CssSelector("img[alt=\"Le Freak\"]")).Click();
-            _driver.FindElement(By.LinkText("Add to cart")).Click();
-            _driver.FindElement(By.LinkText("Checkout >>")).Click();
-            _driver.FindElement(By.Id("FirstName")).Clear();
-            _driver.FindElement(By.Id("FirstName")).SendKeys("Homer");
-            _driver.FindElement(By.Id("LastName")).Clear();
-            _driver.FindElement(By.Id("LastName")).SendKeys("Simpson");
-            _driver.FindElement(By.Id("Address")).Clear();
-            _driver.FindElement(By.Id("Address")).SendKeys("742 Evergreen Terrace");
-            _driver.FindElement(By.Id("City")).Clear();
-            _driver.FindElement(By.Id("City")).SendKeys("Springfield");
-            _driver.FindElement(By.Id("State")).Clear();
-            _driver.FindElement(By.Id("State")).SendKeys("Kentucky");
-            _driver.FindElement(By.Id("PostalCode")).Clear();
-            _driver.FindElement(By.Id("PostalCode")).SendKeys("123456");
-            _driver.FindElement(By.Id("Country")).Clear();
-            _driver.FindElement(By.Id("Country")).SendKeys("United States");
-            _driver.FindElement(By.Id("Phone")).Clear();
-            _driver.FindElement(By.Id("Phone")).SendKeys("2341231241");
-            _driver.FindElement(By.Id("Email")).Clear();
-            _driver.FindElement(By.Id("Email")).SendKeys("chunkylover53@aol.com");
-            _driver.FindElement(By.Id("PromoCode")).Clear();
-            _driver.FindElement(By.Id("PromoCode")).SendKeys("FREE");
-            _driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
+            var driver = SelenoApplicationRunner.Host.Browser;
+            driver.Navigate().GoToUrl(driver.Url);
+            driver.FindElement(By.LinkText("Admin")).Click();
+            driver.FindElement(By.LinkText("Register")).Click();
+            driver.FindElement(By.Id("UserName")).Clear();
+            driver.FindElement(By.Id("UserName")).SendKeys("HJSimpson");
+            driver.FindElement(By.Id("Email")).Clear();
+            driver.FindElement(By.Id("Email")).SendKeys("chunkylover53@aol.com");
+            driver.FindElement(By.Id("Password")).Clear();
+            driver.FindElement(By.Id("Password")).SendKeys("!2345Qwert");
+            driver.FindElement(By.Id("ConfirmPassword")).Clear();
+            driver.FindElement(By.Id("ConfirmPassword")).SendKeys("!2345Qwert");
+            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
+            driver.FindElement(By.LinkText("Disco")).Click();
+            driver.FindElement(By.CssSelector("img[alt=\"Le Freak\"]")).Click();
+            driver.FindElement(By.LinkText("Add to cart")).Click();
+            driver.FindElement(By.LinkText("Checkout >>")).Click();
+            driver.FindElement(By.Id("FirstName")).Clear();
+            driver.FindElement(By.Id("FirstName")).SendKeys("Homer");
+            driver.FindElement(By.Id("LastName")).Clear();
+            driver.FindElement(By.Id("LastName")).SendKeys("Simpson");
+            driver.FindElement(By.Id("Address")).Clear();
+            driver.FindElement(By.Id("Address")).SendKeys("742 Evergreen Terrace");
+            driver.FindElement(By.Id("City")).Clear();
+            driver.FindElement(By.Id("City")).SendKeys("Springfield");
+            driver.FindElement(By.Id("State")).Clear();
+            driver.FindElement(By.Id("State")).SendKeys("Kentucky");
+            driver.FindElement(By.Id("PostalCode")).Clear();
+            driver.FindElement(By.Id("PostalCode")).SendKeys("123456");
+            driver.FindElement(By.Id("Country")).Clear();
+            driver.FindElement(By.Id("Country")).SendKeys("United States");
+            driver.FindElement(By.Id("Phone")).Clear();
+            driver.FindElement(By.Id("Phone")).SendKeys("2341231241");
+            driver.FindElement(By.Id("Email")).Clear();
+            driver.FindElement(By.Id("Email")).SendKeys("chunkylover53@aol.com");
+            driver.FindElement(By.Id("PromoCode")).Clear();
+            driver.FindElement(By.Id("PromoCode")).SendKeys("FREE");
+            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
 
-            Assert.IsTrue(_driver.PageSource.Contains("Checkout Complete"));
+            Assert.IsTrue(driver.PageSource.Contains("Checkout Complete"));
         }
     }
 }
