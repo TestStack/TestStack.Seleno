fe7dc9cc86ee9d4c7bc2739a2e3514ef1b5481a4
MehdiK <me@mehdi-khalili.com>
2012-09-04 09:07:41 +1000

    Added explicit connection string to webconfig and datacontext

--- src/Samples/MusicStore/MvcMusicStore/Models/MusicStoreEntities.cs
+++ src/Samples/MusicStore/MvcMusicStore/Models/MusicStoreEntities.cs
@@ -4,6 +4,11 @@ namespace MvcMusicStore.Models
 {
     public class MusicStoreEntities : DbContext
     {
+        public MusicStoreEntities(): base("DefaultConnection")
+        {
+
+        }
+
         public DbSet<Album> Albums { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Artist> Artists { get; set; }
