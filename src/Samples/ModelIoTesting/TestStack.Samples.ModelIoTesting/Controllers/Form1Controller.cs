2a1561617e1c5e2937b82d0e6832a0f0f27fcce8
Robert Moore <me@robdmoore.id.au>
2013-01-05 11:49:51 +0800

    Added an example form and test data to facilitate testing the model IO

---
+++ src/Samples/ModelIoTesting/TestStack.Samples.ModelIoTesting/Controllers/Form1Controller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Web.Mvc;
+using NUnit.Framework;
+using TestStack.Samples.ModelIoTesting.Fixtures;
+using TestStack.Samples.ModelIoTesting.ViewModels;
+
+namespace TestStack.Samples.ModelIoTesting.Controllers
+{
+    public class Form1Controller : Controller
+    {
+        public ActionResult ExpectFixtureA()
+        {
+            return View("Form", new Form1ViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult ExpectFixtureA(Form1ViewModel vm)
+        {
+            try
+            {
+                Assert.That(vm, IsSame.ViewModelAs(Form1Fixtures.A));
+            }
+            catch (Exception e)
+            {
+                return new ContentResult {Content = e.ToString(), ContentType = "text/plain"};
+            }
+
+            return new EmptyResult();
+        }
+
+        public ActionResult FixtureA()
+        {
+            return View("Form", Form1Fixtures.A);
+        }
+
+    }
+}
