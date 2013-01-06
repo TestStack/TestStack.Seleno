4e2ab4757e8ec6d383ceb5a33b7931f16799fc31
Michael Whelan <mjmwdev@gmail.com>
2013-01-06 10:19:56 +0000

    Added BDDfy/AutoSubstitute tests and converted Franck Theolade's IWebElement tests into that format as examples

---
+++ src/TestStack.Seleno.Tests/Specify/SpecStoryMetaDataScanner.cs
@@ -0,0 +1,19 @@
+using System;
+using TestStack.BDDfy.Core;
+using TestStack.BDDfy.Scanners;
+
+namespace TestStack.Seleno.Tests.Specify
+{
+    public class SpecStoryMetaDataScanner : IStoryMetaDataScanner
+    {
+        public virtual StoryMetaData Scan(object testObject, Type explicityStoryType = null)
+        {
+            var specification = testObject as Specification;
+            if (specification == null)
+                return null;
+
+            var story = new StoryAttribute() {Title = specification.Story.Name};
+            return new StoryMetaData(specification.Story, story);
+        }
+    }
+}
---
+++ src/TestStack.Seleno.Tests/Specify/SpecifySpecs.cs
@@ -0,0 +1,72 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace TestStack.Seleno.Tests.Specify
+{
+    [TestFixture]
+    public class when_creating_a_Specify_specification
+    {
+        [Test]
+        public void should_resolve_the_SUT()
+        {
+            var spec = new TestComponentSpecification();
+            spec.SUT.Should().BeAssignableTo<TestComponent>();
+        }
+
+        [Test]
+        public void should_resolve_the_Substitutes()
+        {
+            var spec = new TestComponentSpecification();
+            spec.SUT.RunAll();
+            spec.Fake<IServiceA>().Received().RunA();
+        }
+
+        [Test]
+        public void should_set_the_correct_story_title()
+        {
+            var scanner = new SpecStoryMetaDataScanner();
+            var metatdata = scanner.Scan(new TestComponentSpecification());
+            metatdata.Title.Should().Be("TestComponent");
+        }
+
+        #region stubs
+
+        private class TestComponentSpecification : SpecificationFor<TestComponent>{}
+
+        public interface IServiceA
+        {
+            void RunA();
+        }
+
+        public interface IServiceB
+        {
+            void RunB();
+        }
+
+        public class ServiceA : IServiceA
+        {
+            public void RunA() { }
+        }
+
+        public sealed class TestComponent
+        {
+            private readonly IServiceA _serviceA;
+            private readonly IServiceB _serviceB;
+
+            public TestComponent(IServiceA serviceA, IServiceB serviceB)
+            {
+                this._serviceA = serviceA;
+                this._serviceB = serviceB;
+            }
+
+            public void RunAll()
+            {
+                this._serviceA.RunA();
+                this._serviceB.RunB();
+            }
+        }
+        #endregion
+
+    }
+}
