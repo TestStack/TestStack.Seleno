11e0ab7381968bc42f8d25a382e37f8828de855c
Robert Moore <me@robdmoore.id.au>
2013-02-06 23:53:16 +0800

    Added Castle.Core logging including configuration of the logger factory via IAppConfigurator Also set some public classes that shouldn't be exposed to internal and added some xml doc comments to some of the interfaces

--- src/TestStack.Seleno.Tests/TestInfrastructure/TestObjectFactory.cs
+++ src/TestStack.Seleno.Tests/TestInfrastructure/TestObjectFactory.cs
@@ -2,24 +2,21 @@
 using TestStack.Seleno.Configuration.Contracts;
 using TestStack.Seleno.Configuration.Screenshots;
 using TestStack.Seleno.Configuration.WebServers;
-using TestStack.Seleno.Infrastructure.Logging.Loggers;
-
 using NSubstitute;
 using OpenQA.Selenium;
 
 namespace TestStack.Seleno.Tests.TestInfrastructure
 {
-    public static class TestObjectFactory
+    internal static class TestObjectFactory
     {
         public static AppConfigurator TestableAppConfigurator()
         {
             var webApplication = new WebApplication(Substitute.For<IProjectLocation>(), 45123);
-            var configurator = new AppConfigurator()
-                .ProjectToTest(webApplication)
+            var configurator = new AppConfigurator();
+            configurator.ProjectToTest(webApplication)
                 .WithWebServer(Substitute.For<IWebServer>())
                 .WithWebDriver(() => Substitute.For<IWebDriver>())
-                .UsingCamera(new NullCamera())
-                .UsingLogger(new NullLogFactory());
+                .UsingCamera(new NullCamera());
 
             return configurator;
         }
--- src/TestStack.Seleno/Configuration/Contracts/IAppConfigurator.cs
+++ src/TestStack.Seleno/Configuration/Contracts/IAppConfigurator.cs
@@ -1,16 +1,52 @@
 using System;
+using Castle.Core.Logging;
 using OpenQA.Selenium;
 using TestStack.Seleno.Configuration.WebServers;
-using TestStack.Seleno.Infrastructure.Logging;
 
 namespace TestStack.Seleno.Configuration.Contracts
 {
+    /// <summary>
+    /// Configure a Seleno test.
+    /// </summary>
     public interface IAppConfigurator
     {
-        AppConfigurator ProjectToTest(WebApplication webApplication);
-        AppConfigurator WithWebServer(IWebServer webServer);
-        AppConfigurator WithWebDriver(Func<IWebDriver> webDriver);
-        AppConfigurator UsingCamera(ICamera camera);
-        AppConfigurator UsingLogger(ILogFactory logFactory);
+        /// <summary>
+        /// Specify the details of the project you are testing.
+        /// </summary>
+        /// <param name="webApplication">The project you are testing</param>
+        /// <returns>The configurator to allow for method chaining</returns>
+        IAppConfigurator ProjectToTest(WebApplication webApplication);
+
+        /// <summary>
+        /// Specify the web server you would like to use.
+        /// By default the IISExpressWebServer is used.
+        /// </summary>
+        /// <param name="webServer">The webserver to use</param>
+        /// <returns>The configurator to allow for method chaining</returns>
+        IAppConfigurator WithWebServer(IWebServer webServer);
+
+        /// <summary>
+        /// Specify the web driver/browser you would like to use.
+        /// By default Firefox is used.
+        /// </summary>
+        /// <param name="webDriver">The web driver</param>
+        /// <returns>The configurator to allow for method chaining</returns>
+        IAppConfigurator WithWebDriver(Func<IWebDriver> webDriver);
+
+        /// <summary>
+        /// Specify the camera you would like to use.
+        /// By default no camera is used.
+        /// </summary>
+        /// <param name="camera">The camera</param>
+        /// <returns>The configurator to allow for method chaining</returns>
+        IAppConfigurator UsingCamera(ICamera camera);
+
+        /// <summary>
+        /// Specify the logger factory you would like to use.
+        /// By default a null logger is used.
+        /// </summary>
+        /// <param name="loggerFactory">The logger factory</param>
+        /// <returns></returns>
+        IAppConfigurator UsingLoggerFactory(ILoggerFactory loggerFactory);
     }
 }
