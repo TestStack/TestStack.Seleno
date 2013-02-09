551493e27229fb918233a0a8b4acc14412faee4d
Robert Moore <me@robdmoore.id.au>
2013-02-09 17:30:39 +0800

    Added check to ensure people don't new-up page objects We can potentially remove these after the project is more bedded down?

--- src/TestStack.Seleno/PageObjects/UiComponent.cs
+++ src/TestStack.Seleno/PageObjects/UiComponent.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using System;
+using OpenQA.Selenium;
 using TestStack.Seleno.Configuration.Contracts;
 using TestStack.Seleno.PageObjects.Actions;
 using TestStack.Seleno.Specifications.Assertions;
@@ -17,33 +18,45 @@ namespace TestStack.Seleno.PageObjects
 
         protected IPageNavigator Navigate()
         {
+            ThrowIfComponentNotCreatedCorrectly();
             return PageNavigator;
         }
 
         protected IScriptExecutor Execute()
         {
+            ThrowIfComponentNotCreatedCorrectly();
             return ScriptExecutor;
         }
 
         protected IElementFinder Find()
         {
+            ThrowIfComponentNotCreatedCorrectly();
             return ElementFinder;
         }
 
         protected TableReader<TModel> TableFor<TModel>(string gridId) where TModel : class, new()
         {
+            ThrowIfComponentNotCreatedCorrectly();
             return new TableReader<TModel>(gridId) { Browser = Browser };
         }
 
         public ElementAssert AssertThatElements(By selector)
         {
+            ThrowIfComponentNotCreatedCorrectly();
             return new ElementAssert(selector, Camera, Browser);
         }
 
         public TComponent GetComponent<TComponent>()
             where TComponent : UiComponent, new()
         {
+            ThrowIfComponentNotCreatedCorrectly();
             return ComponentFactory.CreatePage<TComponent>();
+        }
+
+        private void ThrowIfComponentNotCreatedCorrectly()
+        {
+            if (PageNavigator == null)
+                throw new InvalidOperationException("Don't new up Page Objects; instead use SelenoHost.NavigateToInitialPage");
         }
     }
 }
