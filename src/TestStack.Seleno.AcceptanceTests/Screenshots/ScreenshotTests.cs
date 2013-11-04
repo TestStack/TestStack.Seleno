b90e8ca1272a00445af918f1b1abc02785b52514
Robert Moore <me@robdmoore.id.au>
2013-11-04 22:36:22 +0800

    Added an explicit test that can be run to do a full acceptance test of the file screenshot capability

---
+++ src/TestStack.Seleno.AcceptanceTests/Screenshots/ScreenshotTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Castle.Core.Logging;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using TestStack.Seleno.Configuration;
+using TestStack.Seleno.Configuration.Contracts;
+using TestStack.Seleno.Configuration.WebServers;
+using TestStack.Seleno.PageObjects;
+
+namespace TestStack.Seleno.AcceptanceTests.Screenshots
+{
+    class ScreenshotTests
+    {
+        [Explicit]
+        [Test]
+        [ExpectedException(typeof(NoSuchElementException))]
+        public void TakeScreenshot()
+        {
+            using (var host = new SelenoHost())
+            {
+                host.Run(x => x
+                    .UsingCamera(@"c:\screenshots")
+                    .WithWebServer(new InternetWebServer("http://www.google.com/"))
+                    .WithMinimumWaitTimeoutOf(TimeSpan.FromMilliseconds(100))
+                    .UsingLoggerFactory(new ConsoleFactory(LoggerLevel.Debug))
+                );
+
+                host.NavigateToInitialPage<Page>()
+                    .Find.Element(By.Name("doesntexist"));
+            }
+        }
+    }
+}
