88c5cfe6020fbc71436206eeb9a523ce5011b9aa
Robert Moore <me@robdmoore.id.au>
2013-11-17 15:43:09 +0800

    Abstracted the textbox control test

--- src/TestStack.Seleno.Tests/PageObjects/Actions/Controls/When_updating_TextBox_value.cs
+++ src/TestStack.Seleno.Tests/PageObjects/Actions/Controls/When_updating_TextBox_value.cs
@@ -1,45 +1,72 @@
 using System;
 using System.Globalization;
+using System.Linq.Expressions;
 using NSubstitute;
 using TestStack.Seleno.PageObjects.Controls;
+using TestStack.Seleno.Tests.TestObjects;
 
 namespace TestStack.Seleno.Tests.PageObjects.Actions.Controls
 {
-    class When_updating_textbox_value_with_date : HtmlControlSpecificationFor<TextBox>
+    abstract class TextboxControlSpecification<TValueType> : HtmlControlSpecificationFor<TextBox>
     {
-        private static readonly DateTime _the03rdOfJanuary2012At21h21 = new DateTime(2012, 01, 03, 21, 21, 00);
-        private readonly string _expectedScriptToBeExecuted =
-            string.Format(@"$('#Modified').val(""{0}"")",_the03rdOfJanuary2012At21h21.ToString(CultureInfo.CurrentCulture));
-
-        public When_updating_textbox_value_with_date() : base(x => x.Modified) { }
+        protected TextboxControlSpecification(Expression<Func<TestViewModel, object>> htmlControlPropertySelector)
+            : base(htmlControlPropertySelector) {}
+        protected abstract TValueType Value { get; }
+        protected abstract string ExpectedControlId { get; }
+        protected abstract string ExpectedControlValue { get; }
 
         public void When_updating_the_textbox_value()
         {
-            SUT.ReplaceInputValueWith(_the03rdOfJanuary2012At21h21);
+            SUT.ReplaceInputValueWith(Value);
         }
 
         public void Then_script_executor_should_execute_relevant_script_to_replace_the_value()
         {
             Executor
                 .Received()
-                .Script(_expectedScriptToBeExecuted);
+                .Script(string.Format("$('#{0}').val(\"{1}\")", ExpectedControlId, ExpectedControlValue));
         }
     }
 
-    class When_updating_textbox_value_with_string : HtmlControlSpecificationFor<TextBox>
+    class When_updating_textbox_value_with_date : TextboxControlSpecification<DateTime>
+    {
+        private static readonly DateTime ThirdOfJanuary2012AtNineTwentyOnePm = new DateTime(2012, 01, 03, 21, 21, 00);
+
+        public When_updating_textbox_value_with_date() : base(x => x.Modified) { }
+
+        protected override DateTime Value
+        {
+            get { return ThirdOfJanuary2012AtNineTwentyOnePm; }
+        }
+
+        protected override string ExpectedControlId
+        {
+            get { return "Modified"; }
+        }
+
+        protected override string ExpectedControlValue
+        {
+            get { return ThirdOfJanuary2012AtNineTwentyOnePm.ToString(CultureInfo.CurrentCulture); }
+        }
+    }
+
+    class When_updating_textbox_value_with_string : TextboxControlSpecification<string>
     {
         public When_updating_textbox_value_with_string() : base(x => x.Modified) { }
 
-        public void When_updating_the_textbox_value()
+        protected override string Value
         {
-            SUT.ReplaceInputValueWith("asdf \\ \" \r\n");
+            get { return "asdf \\ \" \r\n"; }
         }
 
-        public void Then_script_executor_should_execute_relevant_script_to_replace_the_value()
+        protected override string ExpectedControlId
         {
-            Executor
-                .Received()
-                .Script(@"$('#Modified').val(""asdf \\ \"" \r\n"")");
+            get { return "Modified"; }
+        }
+
+        protected override string ExpectedControlValue
+        {
+            get { return @"asdf \\ \"" \r\n"; }
         }
     }
 }
