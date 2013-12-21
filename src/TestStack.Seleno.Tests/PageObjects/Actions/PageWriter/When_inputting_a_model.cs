67c2a552ddb8887b3a1af6b8f3c205811d3376a0
Robert Moore <me@robdmoore.id.au>
2013-12-21 17:30:28 +0800

    Refactored the Input.Model tests so that they are more resilient (dropped the magic strings)

--- src/TestStack.Seleno.Tests/PageObjects/Actions/PageWriter/When_inputting_a_model.cs
+++ src/TestStack.Seleno.Tests/PageObjects/Actions/PageWriter/When_inputting_a_model.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using FizzWare.NBuilder;
+using TestStack.Seleno.Configuration.Contracts;
 using TestStack.Seleno.Configuration.ControlIdGenerators;
 using TestStack.Seleno.PageObjects;
 using TestStack.Seleno.PageObjects.Actions;
@@ -17,6 +18,7 @@ namespace TestStack.Seleno.Tests.PageObjects.Actions.PageWriter
         private const string DateFormat = "d/M/yyyy";
         private TestViewModel _model;
         private Dictionary<Type, Func<object, string>> _propertyHandling;
+        private readonly IControlIdGenerator _controlIdGenerator = new MvcControlIdGenerator();
 
         public void AndGiven_property_handling_setup_for_datetime()
         {
@@ -40,87 +42,95 @@ namespace TestStack.Seleno.Tests.PageObjects.Actions.PageWriter
 
         public void Then_input_string_property()
         {
-            AssertPropertyValueSet("Name", _model.Name);
+            AssertPropertyValueSet(m => m.Name);
         }
 
         public void And_input_datetime_property_using_property_type_handling()
         {
-            AssertPropertyValueSet("Modified", _model.Modified.ToString(DateFormat));
+            AssertPropertyValueSet(m => m.Modified);
         }
 
         public void And_ignore_hidden_input_property()
         {
-            AssertPropertyIgnored("HiddenProperty");
+            AssertPropertyIgnored(m => m.HiddenProperty);
         }
 
         public void And_ignore_readonly_property()
         {
-            AssertPropertyIgnored("ReadonlyProperty");
+            AssertPropertyIgnored(m => m.ReadonlyProperty);
         }
 
         public void And_input_non_readonly_property()
         {
-            AssertPropertyValueSet("NonReadonlyProperty", _model.NonReadonlyProperty);
+            AssertPropertyValueSet(m => m.NonReadonlyProperty);
         }
 
         public void And_input_scaffolded_property()
         {
-            AssertPropertyValueSet("ScaffoldedProperty", _model.ScaffoldedProperty);
+            AssertPropertyValueSet(m => m.ScaffoldedProperty);
         }
 
         public void And_ignore_non_scaffolded_property()
         {
-            AssertPropertyIgnored("NonScaffoldedProperty");
+            AssertPropertyIgnored(m => m.NonScaffoldedProperty);
         }
 
         public void And_input_sub_view_model_string_property()
         {
-            AssertPropertyValueSet("SubViewModel_Name", _model.SubViewModel.Name);
+            AssertPropertyValueSet(m => m.SubViewModel.Name);
         }
 
         public void And_input_sub_view_model_datetime_property_using_property_type_handling()
         {
-            AssertPropertyValueSet("SubViewModel_Modified", _model.Modified.ToString(DateFormat));
+            AssertPropertyValueSet(m => m.SubViewModel.Modified);
         }
 
         public void And_ignore_sub_view_model_hidden_input_property()
         {
-            AssertPropertyIgnored("SubViewModel_HiddenProperty");
+            AssertPropertyIgnored(m => m.SubViewModel.HiddenProperty);
         }
 
         public void And_ignore_sub_view_model_readonly_property()
         {
-            AssertPropertyIgnored("SubViewModel_ReadonlyProperty");
+            AssertPropertyIgnored(m => m.SubViewModel.ReadonlyProperty);
         }
 
         public void And_input_sub_view_model_non_readonly_property()
         {
-            AssertPropertyValueSet("SubViewModel_NonReadonlyProperty", _model.SubViewModel.NonReadonlyProperty);
+            AssertPropertyValueSet(m => m.SubViewModel.NonReadonlyProperty);
         }
 
         public void And_input_sub_view_model_scaffolded_property()
         {
-            AssertPropertyValueSet("SubViewModel_ScaffoldedProperty", _model.SubViewModel.ScaffoldedProperty);
+            AssertPropertyValueSet(m => m.SubViewModel.ScaffoldedProperty);
         }
 
         public void And_ignore_sub_view_model_non_scaffolded_property()
         {
-            AssertPropertyIgnored("SubViewModel_NonScaffoldedProperty");
+            AssertPropertyIgnored(m => m.SubViewModel.NonScaffoldedProperty);
         }
 
         public void And_input_deep_nested_model_property()
         {
-            AssertPropertyValueSet("SubViewModel_SubViewModel_Name", _model.SubViewModel.SubViewModel.Name);
-        }
-        
-        private void AssertPropertyIgnored(string fieldId)
-        {
-            SubstituteFor<IExecutor>().DidNotReceive().Script(Arg.Is<string>(s => s.Contains("#" + fieldId)));
+            AssertPropertyValueSet(m => m.SubViewModel.SubViewModel.Name);
         }
 
-        private void AssertPropertyValueSet(string fieldId, string fieldValue)
+        private void AssertPropertyIgnored<TProperty>(Expression<Func<TestViewModel, TProperty>> property)
         {
-            SubstituteFor<IExecutor>().Received().Script(string.Format("$('#{0}').val(\"{1}\")", fieldId, fieldValue.ToJavaScriptString()));
+            var expectedId = _controlIdGenerator.GetControlId(_controlIdGenerator.GetControlName(property));
+            SubstituteFor<IExecutor>().DidNotReceive().Script(Arg.Is<string>(s => s.Contains("#" + expectedId)));
+        }
+
+        private void AssertPropertyValueSet<TProperty>(Expression<Func<TestViewModel, TProperty>> property)
+        {
+            var expectedId = _controlIdGenerator.GetControlId(_controlIdGenerator.GetControlName(property));
+            var propertyValue = property.Compile().Invoke(_model);
+
+            var expectedValue = propertyValue.ToString();
+            if (propertyValue is DateTime)
+                expectedValue = ((DateTime)(object)propertyValue).ToString(DateFormat);
+
+            SubstituteFor<IExecutor>().Received().Script(string.Format("$('#{0}').val(\"{1}\")", expectedId, expectedValue.ToJavaScriptString()));
         }
 
         public override void Setup()
@@ -130,7 +140,7 @@ namespace TestStack.Seleno.Tests.PageObjects.Actions.PageWriter
                 {
                     PageNavigator = SubstituteFor<IPageNavigator>(),
                     Executor = SubstituteFor<IExecutor>(),
-                    ControlIdGenerator = new MvcControlIdGenerator()
+                    ControlIdGenerator = _controlIdGenerator
                 }
                     .Initialize(a.Arg<LambdaExpression>())
                 );
