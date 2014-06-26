c59223956321f1988e61dd5339c1d6f7a26bf8cb
Stefano Germani <stefano.germani@thetrainline.com>
2014-06-26 10:30:57 +0100

    Added tests to Textbox.Text property

---
+++ src/TestStack.Seleno.Tests/PageObjects/Actions/Controls/When_retrieving_textbox_text.cs
@@ -0,0 +1,25 @@
+using System;
+using FluentAssertions;
+using NSubstitute;
+using OpenQA.Selenium;
+using TestStack.Seleno.PageObjects.Controls;
+
+namespace TestStack.Seleno.Tests.PageObjects.Actions.Controls
+{
+    class When_retrieving_textbox_text : HtmlControlSpecificationFor<TextBox, string>
+    {
+        public When_retrieving_textbox_text() : base(x => x.Name) { }
+
+        public void When_retrieving_the_textbox_text()
+        {
+            var temp = SUT.Text;
+        }
+
+        public void Then_it_should_return_the_correct_content()
+        {
+            Executor
+                .Received()
+                .ScriptAndReturn<string>("$('#Name').val()");
+        }
+    }
+}
---
+++ src/TestStack.Seleno.Tests/PageObjects/Actions/Controls/When_updating_textbox_text.cs
@@ -0,0 +1,26 @@
+using NSubstitute;
+using TestStack.Seleno.PageObjects.Controls;
+
+namespace TestStack.Seleno.Tests.PageObjects.Actions.Controls
+{
+    internal class When_updating_textbox_text : HtmlControlSpecificationFor<TextBox, string>
+    {
+        private const string Text = "textbox content";
+
+        public When_updating_textbox_text() : base(x => x.Name)
+        {
+        }
+
+        public void When_updating_the_textbox_value()
+        {
+            SUT.Text = Text;
+        }
+
+        public void Then_script_executor_should_execute_relevant_script_to_replace_the_value()
+        {
+            Executor
+                .Received()
+                .Script(string.Format("$('#Name').val(\"{0}\")", Text));
+        }
+    }
+}
