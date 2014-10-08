a24457b577d74a79c2f60ed3bc642df28504101e
Dennis Roche <dennis.roche@readify.net>
2014-10-08 12:49:57 +0800

    Moved SelenoReceivedException to separate file

--- src/TestStack.Seleno/Configuration/Interceptors/CameraProxyInterceptor.cs
+++ src/TestStack.Seleno/Configuration/Interceptors/CameraProxyInterceptor.cs
@@ -51,18 +51,4 @@ namespace TestStack.Seleno.Configuration.Interceptors
             }
         }
     }
-    
-    /// <summary>
-    /// Wraps an exception that has been received and processed by Seleno.
-    /// </summary>
-    public class SelenoReceivedException : Exception
-    {
-        /// <summary>
-        /// Creates a <see cref="SelenoReceivedException"/>.
-        /// </summary>
-        /// <param name="innerException">The exception that has been caught and processed by Seleno</param>
-        public SelenoReceivedException(Exception innerException)
-            : base(innerException.Message, innerException)
-        {}
-    }
 }
