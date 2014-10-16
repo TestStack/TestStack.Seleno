179ac937b8c5db9c1a96c73c69405868652483c5
Dennis Roche <dennis.roche@readify.net>
2014-10-16 17:37:32 +0800

    Changed registration to use CreateInterfaceProxyWithTarget overload accepting multiple interceptors, i.e. creating a pipeline of interceptors.

--- src/TestStack.Seleno/Configuration/Interceptors/DomCaptureProxyInterceptor.cs
+++ src/TestStack.Seleno/Configuration/Interceptors/DomCaptureProxyInterceptor.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Castle.Core.Logging;
 using Castle.DynamicProxy;
 using TestStack.Seleno.Configuration.Contracts;
