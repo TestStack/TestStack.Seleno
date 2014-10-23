317c1aed7ac3a8bb2126cc523fd5be8510bc586c
mattcowen <mattgcowen@gmail.com>
2014-10-23 11:25:11 +0100

    Revert "#181 proposed change to use the name not id"
    This reverts commit e0e334ad991e73925f477190625ecf787a8899bb.

--- src/TestStack.Seleno/PageObjects/Controls/InputHtmlControl.cs
+++ src/TestStack.Seleno/PageObjects/Controls/InputHtmlControl.cs
@@ -8,16 +8,16 @@ namespace TestStack.Seleno.PageObjects.Controls
         {
             get { return ValueAs<string>(); }
         }
-
+        
         public virtual TReturn ValueAs<TReturn>()
         {
-            var scriptToExecute = string.Format(@"$('[name=""{0}""').val()", Name);
+            var scriptToExecute = string.Format("$('#{0}').val()", Id);
             return Execute.ScriptAndReturn<TReturn>(scriptToExecute);
         }
 
         public void ReplaceInputValueWith<TProperty>(TProperty inputValue)
         {
-            var scriptToExecute = string.Format(@"$('[name=""{0}""]').val(""{1}"")", Name, inputValue.ToString().ToJavaScriptString());
+            var scriptToExecute = string.Format(@"$('#{0}').val(""{1}"")", Id, inputValue.ToString().ToJavaScriptString());
             Execute.Script(scriptToExecute);
         }
     }
