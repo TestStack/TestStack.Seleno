6f2ea3f3ceb535a416a0c234c547dc8ee8d78981
MehdiK <me@mehdi-khalili.com>
2014-10-26 21:32:55 +1000

    Installed mvc nuget & updated a few references

--- src/Samples/MusicStore/MvcMusicStore/Models/AccountModels.cs
+++ src/Samples/MusicStore/MvcMusicStore/Models/AccountModels.cs
@@ -19,7 +19,7 @@ namespace MvcMusicStore.Models
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        [System.ComponentModel.DataAnnotations.Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
 
@@ -57,7 +57,7 @@ namespace MvcMusicStore.Models
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
 }
